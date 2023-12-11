988bc21256c683c00fa3878cf348d8b85d66db72
hetansh-Tatvasoft <hetansh.patel@tatvasoft.com>
2023-12-11 12:14:02 +0530

    FrontEnd app setup.

--- FS_h/backEnd/app.js
+++ FS_h/backEnd/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("<h1>WhatsApp Bitches...</h1>");
+  res.send("<h1>WhatsApp...</h1>");
 });
 
 app.listen(3000, () => {
